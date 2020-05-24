fa71097c28dbfe817e35bd5c5223419e92aaf5a4
Bao <baoit128@gmail.com>
2020-05-24 10:04:13 +0700

    add config

---
+++ src/configs/client.js
@@ -0,0 +1,13 @@
+export const ACCESS_TOKEN_KEY = 'accesstoken';
+export const REFRESH_TOKEN_KEY = 'refreshtoken';
+export const EMAIL_KEY = 'email';
+
+export const MENUITEM = {
+    PAYMENT_ACCOUNTS: 'payment-accounts',
+    INTERNAL_TRANSFER: 'internal-transfer',
+    SETUP_RECIPIENT: 'setup-recipient',
+    TRANSACTION_HISTORY: 'transaction-history',
+    CLOSE_WALLET: 'close-wallet',
+};
+
+export const OTP_EMAIL = 'otp-email';
